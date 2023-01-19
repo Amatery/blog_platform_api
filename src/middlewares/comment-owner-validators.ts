08c841c239fd38dedec6b0e78cb667c5e4f9dbf6
Alexey Lesik <sindzi0007@proton.me>
2023-01-19 19:15:03 +0300

    comment owner validator fixes

--- src/middlewares/comment-owner-validators.ts
+++ src/middlewares/comment-owner-validators.ts
@@ -1,5 +1,6 @@
 import { param } from 'express-validator'
 import { commentsService } from '../domain/comments-service'
+import { STATUS_CODES } from '../helpers/StatusCodes'
 import { CommentViewModel } from '../Models/CommentsModels/CommentViewModel'
 
 export const validateCommentOwner = param('id')
@@ -8,9 +9,12 @@ export const validateCommentOwner = param('id')
   .isLength({ min: 1 })
   .custom(async (v, { req }) => {
     const foundComment: CommentViewModel | null = await commentsService.getCommentById(v)
-    if (foundComment === null || foundComment.userId !== req.user.userId) {
-      return Promise.reject('Forbidden')
-    } else {
+    if (foundComment === null) {
+      return Promise.reject(STATUS_CODES.NOT_FOUND)
+    }
+    if (foundComment.userId === req.user.id) {
       return true
+    } else {
+      return Promise.reject(STATUS_CODES.FORBIDDEN)
     }
   })
--- src/middlewares/owner-validation-middleware.ts
+++ src/middlewares/owner-validation-middleware.ts
@@ -9,7 +9,10 @@ export const ownerValidationMiddleware = (
 ) => {
   const errors = validationResult(req)
   if (!errors.isEmpty()) {
-    res.sendStatus(STATUS_CODES.FORBIDDEN)
+    const error = errors.array({ onlyFirstError: true }).map(e => e.msg)
+    error[0] === STATUS_CODES.NOT_FOUND ?
+      res.sendStatus(STATUS_CODES.NOT_FOUND) :
+      res.sendStatus(STATUS_CODES.FORBIDDEN)
     return
   } else {
     next()
