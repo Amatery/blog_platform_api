f7fa19930c454a1de93b9045df3c6e7f50fd5aa5
Aliaksei Lesik <aliaksei.lesik@proton.me>
2024-02-29 20:10:36 +0300

    minor fixes

--- src/repositories/blogs-repository.ts
+++ src/repositories/blogs-repository.ts
@@ -1,14 +1,14 @@
-import { DeleteResult } from 'mongodb'
-import { v4 as uuidv4 } from 'uuid'
-import { blogsCollection, postsCollection } from '../database/database-config'
-import { getBlogPaginationModel } from '../helpers/getBlogPaginationModel'
-import { getBlogViewModel } from '../helpers/getBlogViewModel'
-import { getPostPaginationModel } from '../helpers/getPostPaginationModel'
-import { getPostViewModel } from '../helpers/getPostViewModel'
-import { BlogViewModel } from '../models/BlogModels/BlogViewModel'
-import { PaginationBlogModel } from '../models/BlogModels/PaginationBlogModel'
-import { PaginationPostModel } from '../models/PostModels/PaginationPostModel'
-import { PostViewModel } from '../models/PostModels/PostViewModel'
+import { DeleteResult } from 'mongodb';
+import { v4 as uuidv4 } from 'uuid';
+import { blogsCollection, postsCollection } from '../database/database-config';
+import { getBlogPaginationModel } from '../helpers/getBlogPaginationModel';
+import { getBlogViewModel } from '../helpers/getBlogViewModel';
+import { getPostPaginationModel } from '../helpers/getPostPaginationModel';
+import { getPostViewModel } from '../helpers/getPostViewModel';
+import { BlogViewModel } from '../models/BlogModels/BlogViewModel';
+import { PaginationBlogModel } from '../models/BlogModels/PaginationBlogModel';
+import { PaginationPostModel } from '../models/PostModels/PaginationPostModel';
+import { PostViewModel } from '../models/PostModels/PostViewModel';
 
 export const blogsRepository = {
   async getBlogs(
@@ -21,25 +21,25 @@ export const blogsRepository = {
     const blogs = await blogsCollection.find({
         name: {
           $regex: searchNameTerm ?? '',
-          $options: '-i',
+          $options: 'i',
         },
       })
       .sort({ [sortBy]: sortDirection === 'desc' ? -1 : 1 })
       .skip(pageSize * (pageNumber - 1))
       .limit(pageSize)
-      .toArray()
-    const totalDocuments = await blogsCollection.countDocuments({})
-    const totalItems = searchNameTerm !== null ? blogs.length : totalDocuments
-    const pagesCount = Math.ceil(totalItems / pageSize)
-    return getBlogPaginationModel(pageSize, pageNumber, pagesCount, totalItems, blogs)
+      .toArray();
+    const totalDocuments = await blogsCollection.countDocuments({});
+    const totalItems = searchNameTerm !== null ? blogs.length : totalDocuments;
+    const pagesCount = Math.ceil(totalItems / pageSize);
+    return getBlogPaginationModel(pageSize, pageNumber, pagesCount, totalItems, blogs);
   },
   async getBlogById(id: string): Promise<BlogViewModel | null> {
-    const foundBlog = await blogsCollection.findOne({ id })
-    return foundBlog === null ? null : getBlogViewModel(foundBlog)
+    const foundBlog = await blogsCollection.findOne({ id });
+    return foundBlog === null ? null : getBlogViewModel(foundBlog);
   },
   async createBlog(newBlog: BlogViewModel): Promise<BlogViewModel> {
-    await blogsCollection.insertOne(newBlog)
-    return getBlogViewModel(newBlog)
+    await blogsCollection.insertOne(newBlog);
+    return getBlogViewModel(newBlog);
   },
   async updateBlogById(id: string, name: string, description: string, websiteUrl: string): Promise<boolean> {
     const foundBlog = await blogsCollection.updateOne(
@@ -51,12 +51,12 @@ export const blogsRepository = {
           websiteUrl,
         },
       },
-    )
-    return foundBlog.matchedCount === 1
+    );
+    return foundBlog.matchedCount === 1;
   },
   async deleteBlogById(id: string): Promise<boolean> {
-    const foundBlog = await blogsCollection.deleteOne({ id })
-    return foundBlog.deletedCount === 1
+    const foundBlog = await blogsCollection.deleteOne({ id });
+    return foundBlog.deletedCount === 1;
   },
   async findPostsByBlogId(
     blogId: string,
@@ -66,16 +66,16 @@ export const blogsRepository = {
     pageNumber: number,
     pageSize: number,
   ): Promise<PaginationPostModel | null> {
-    const totalCount = await postsCollection.countDocuments({ blogId })
-    const pagesCount = Math.ceil(totalCount / pageSize)
+    const totalCount = await postsCollection.countDocuments({ blogId });
+    const pagesCount = Math.ceil(totalCount / pageSize);
     const foundPosts = await postsCollection.find({ blogId })
       .sort({ [sortBy]: sortDirection === 'desc' ? -1 : 1 })
       .skip(pageSize * (pageNumber - 1))
       .limit(pageSize)
-      .toArray()
+      .toArray();
     return !foundPosts.length ?
       null :
-      getPostPaginationModel(pageSize, pageNumber, pagesCount, totalCount, foundPosts)
+      getPostPaginationModel(pageSize, pageNumber, pagesCount, totalCount, foundPosts);
 
   },
   async createPostByBlogId(
@@ -84,7 +84,7 @@ export const blogsRepository = {
     shortDescription: string,
     content: string,
   ): Promise<PostViewModel | null> {
-    const foundBlog = await blogsCollection.findOne({ id: blogId })
+    const foundBlog = await blogsCollection.findOne({ id: blogId });
     if (foundBlog) {
       const createdPost: PostViewModel = {
         id: uuidv4(),
@@ -94,11 +94,11 @@ export const blogsRepository = {
         blogId,
         blogName: foundBlog.name,
         createdAt: new Date().toISOString(),
-      }
-      await postsCollection.insertOne(createdPost)
-      return getPostViewModel(createdPost)
+      };
+      await postsCollection.insertOne(createdPost);
+      return getPostViewModel(createdPost);
     } else {
-      return null
+      return null;
     }
   },
 
@@ -106,10 +106,10 @@ export const blogsRepository = {
    * ONLY FOR E2E TESTS
    */
   async _deleteAllBlogs(): Promise<DeleteResult> {
-    return blogsCollection.deleteMany({})
+    return blogsCollection.deleteMany({});
   },
   /**
    *
    */
 
-}
+};
