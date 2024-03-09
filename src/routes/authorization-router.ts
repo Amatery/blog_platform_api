12da9e082c5bc84530a5c2a5422382b8de54e37f
Aliaksei Lesik <aliaksei.lesik@proton.me>
2024-03-09 15:36:25 +0300

    Revert "recovery password flow fix 4"
    This reverts commit ee8b3b697a772328cfeff2065fbb5421c68f0f70.

--- src/routes/authorization-router.ts
+++ src/routes/authorization-router.ts
@@ -91,7 +91,6 @@ authorizationRouter.post(
   inputValidationMiddleware,
   async (req: RequestWithBody<PasswordRecoveryInputModel>, res: Response) => {
     const { email } = req.body;
-    console.log('router email', email);
     const result = await authorizationService.sendRecoveryPasswordEmail(email);
     if (!result) {
       res.sendStatus(STATUS_CODES.NO_CONTENT);
